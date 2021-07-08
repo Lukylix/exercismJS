a634a857f3717fc878f7ef7fca9163b06227f85a
Lukylix <lucas.garcia.bobi@gmail.com>
2021-07-08 14:30:15 +0200

    Matrix using map

--- matrix/matrix.js
+++ matrix/matrix.js
@@ -5,27 +5,8 @@
 
 export class Matrix {
   constructor(string) {
-    this.row = string.split(/\n/gm);
-    for (let i = 0; i < this.row.length; i++) {
-      this.row[i] = this.row[i].split(" ");
-      //Int converstion
-      for (let x = 0; x < this.row[i].length; x++) this.row[i][x] = parseInt(this.row[i][x]);
-    }
+    this.rows= string.split(/\n/gm).map((e) => e.split(" ").map((e) => parseInt(e)));
 
-    this.column = [];
-    this.row[0].forEach(() => {
-      this.column.push([]);
-    });
-    for (let j = 0; j < this.row.length; j++) {
-      for (let k = 0; k < this.row[j].length; k++) this.column[k].push(this.row[j][k]);
-    }
-  }
-
-  get rows() {
-    return this.row;
-  }
-
-  get columns() {
-    return this.column;
+    this.columns = this.rows[0].map((col,i) => this.rows.map(row => row[i]))
   }
 }
